b394444ca67e67c44c1ebab5e63c5bb86c8a3fd2
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2020-08-10 15:18:24 +0300

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'videoko-test.devarena.lviv.ua', protocol: 'http' }
   config.action_mailer.asset_host = 'http://videoko-test.devarena.lviv.ua:3000'
-  config.action_controller.asset_host = 'http://videoko-test.devarena.lviv.ua:3000'
-
+  
   config.action_mailer.smtp_settings = {
   :address => Rails.application.credentials[:aws][:address],
   :port => 25,
