b38a658b2aa5d71663858a163f9047dc768009ba
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2021-02-23 14:59:00 +0200

    fix mail

--- app/mailers/mutual_settlement_request_mailer.rb
+++ app/mailers/mutual_settlement_request_mailer.rb
@@ -2,6 +2,7 @@ class MutualSettlementRequestMailer < ApplicationMailer
   def inform_admin_mutual_settlement(user, message)
     @user_name = Spree::User.find(user).full_name
     @message = message
-    mail(to: "kinzhuvatova@gmail.com", from: "videoko2016@gmail.com", subject: "")
+    email = Spree::User.find(user).email
+    mail(to: email, from: "videoko2016@gmail.com", subject: "")
   end
 end
